6cfd268b0bf4749505ec10d0ed08d90e63e7300f
KOREKOU EMMANUEL GBRA <92445623+Losahfirst@users.noreply.github.com>
2025-04-15 18:14:45 +0000

    Update exercixe2.c

--- exercixe2.c
+++ exercixe2.c
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+
 
 int main()
 {
@@ -7,26 +9,24 @@ int main()
     float conso_moyenne;
     int jour_max = 0;
     float max_conso = 0;
-    
-    char *day[7] = {"lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"};
 
     for (int i = 0; i < 7; i++) {
-        printf("Saisissez votre consommation du %s : ", day[i]);
+        printf("Saisissez votre consommation du jour J%d : ", i + 1);
         scanf("%f", &conso[i]);
 
         conso_total += conso[i];
 
         if (conso[i] > max_conso) {
             max_conso = conso[i];
-            jour_max = i;
+            jour_max = i + 1;
         }
     }
 
     conso_moyenne = conso_total / 7;
 
-    printf("\nConsommation totale : %.2f kWh\n", conso_total);
-    printf("Consommation moyenne : %.2f kWh\n", conso_moyenne);
-    printf("Jour de consommation la plus élevée : %s (%.2f kWh)\n", day[jour_max], max_conso);
+    printf("\nConsommation totale : %.2f\n", conso_total);
+    printf("Consommation moyenne : %.2f\n", conso_moyenne);
+    printf("Jour de consommation la plus élevée : J%d (%.2f) kWh", jour_max, max_conso);
 
     return 0;
 }
